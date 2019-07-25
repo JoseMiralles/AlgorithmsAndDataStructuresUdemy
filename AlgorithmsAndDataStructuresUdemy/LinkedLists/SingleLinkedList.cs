14647de0127c6d08f783862f405073f20c4b905a
Jose Miralles <josemiralles1991@gmail.com>
2019-07-25 12:15:22 -0400

    Added generic type constraint to list

--- AlgorithmsAndDataStructuresUdemy/LinkedLists/SingleLinkedList.cs
+++ AlgorithmsAndDataStructuresUdemy/LinkedLists/SingleLinkedList.cs
@@ -5,107 +5,10 @@ using System.Collections.Generic;
 namespace AlgorithmsAndDataStructuresUdemy
 {
     /// <summary>
-    /// A single linked list with some common functionality.
-    /// </summary>
-    /// <typeparam name="T">Generic Type</typeparam>
-    public class SingleLinkedListOld<T>
-    {
-        public Node<T> head;
-
-        public SingleLinkedListOld(Node<T> head = null)
-        {
-            this.head = head;
-        }
-
-        public Node<T> Find(Node<T> item)
-        {
-            Node<T> current = this.head;
-            while (current != null)
-            {
-                current = current.next;
-                //TODO: Compare and return.
-            }
-            return null;
-        }
-
-        /// <summary>
-        /// Traverses the list until the last item.
-        /// </summary>
-        /// <returns>The last item on the list. Or null if the list is empty</returns>
-        public Node<T> GetLastItem()
-        {
-            Node<T> current = this.head;
-            while (current != null)
-            {
-                current = current.next;
-            }
-            return current;
-        }
-
-        /// <summary>
-        /// Traverses the whole list, and prints it's content.
-        /// </summary>
-        public void TraverseAndPrint()
-        {
-            if (head != null)
-            {
-                Node<T> current = head;
-                while (current != null)
-                {
-                    Console.WriteLine(current.data);
-                    current = current.next;
-                }
-            }
-        }
-
-        /// <summary>
-        /// A single link node.
-        /// </summary>
-        public class Node<T>
-        {
-            public Node<T> next;
-            public T data;
-
-            public Node(T data, Node<T> next = null)
-            {
-                this.data = data;
-                this.next = next;
-            }
-
-            /// <summary>
-            /// Checks to see if the items are the same for comparable classes.
-            /// </summary>
-            /// <typeparam name="T"></typeparam>
-            /// <param name="a">first item</param>
-            /// <param name="b">second item</param>
-            /// <returns></returns>
-            public bool Equals<T>(T a, T b) where T : class
-            {
-                if (a == b)
-                    return true;
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Creates a single linked list with three items.
-        /// </summary>
-        public static void CreateLinkedListAndLoop()
-        {
-            SingleLinkedListOld<String> linkedList = new SingleLinkedListOld<String>();
-            linkedList.head = new SingleLinkedListOld<String>.Node<String>("First");
-            linkedList.head.next = new SingleLinkedListOld<String>.Node<String>("Second");
-            linkedList.head.next.next = new SingleLinkedListOld<String>.Node<String>("Third");
-
-            linkedList.TraverseAndPrint();
-        }
-    }
-
-    /// <summary>
     /// Single Linked list that implements the IList<T> interface.
     /// </summary>
     /// <typeparam name="T">The type to hold</typeparam>
-    public class SingleLinkedList<T> : IList<T>
+    public class SingleLinkedList<T> : IList<T> where T : IComparable<T>
     {
         public Node<T> head;
 
@@ -118,12 +21,12 @@ namespace AlgorithmsAndDataStructuresUdemy
         /// <summary>
         /// Traverses the whole list, and prints each item.
         /// </summary>
-        public void TraverseAndPrint()
+        public bool TraverseAndPrint()
         {
             if (head == null)
             {
                 Console.WriteLine("Single Linked List is empty!");
-                return;
+                return false;
             }
             else
             {
@@ -133,6 +36,7 @@ namespace AlgorithmsAndDataStructuresUdemy
                     Console.WriteLine(current.data);
                     current = current.next;
                 }
+                return true;
             }
         }
 
@@ -159,9 +63,17 @@ namespace AlgorithmsAndDataStructuresUdemy
             }
         }
 
+        /// <summary>
+        /// Append an item at the end of the List.
+        /// </summary>
+        /// <param name="item">Item to append</param>
+        public void Append(T item)
+        {
+            this.GetLastItem().next = new Node<T>(item);
+        }
+
         public void Clear()
         {
-            throw new NotImplementedException();
         }
 
         public bool Contains(T item)
@@ -204,37 +116,37 @@ namespace AlgorithmsAndDataStructuresUdemy
             throw new NotImplementedException();
         }
 
+        public Node<T> GetLastItem()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.next == null)
+                    return current;
+                current = current.next;
+            }
+            return current;
+        }
+
         /// <summary>
         /// Single linked node with a reference to the next item.
         /// </summary>
-        public class Node<T>
+        public class Node<T1> where T1 : IComparable<T1>
         {
-            public Node<T> next { get; set; }
-            public T data { get; set; }
+            public Node<T1> next { get; set; }
+            public T1 data { get; set; }
 
             /// <summary>
             /// Single link Node with a reference to the next node.
             /// </summary>
             /// <param name="data">Data to store</param>
             /// <param name="next">The next item on the list</param>
-            public Node (T data, Node<T> next = null)
+            public Node (T1 data, Node<T1> next = null)
             {
                 this.data = data;
                 this.next = next;
             }
         }
-
-        #region Implementation examples.
-        public static SingleLinkedList<string> CreateAndPopulateStringList()
-        {
-            SingleLinkedList<String> list = new SingleLinkedList<string>();
-            for (int i = 0; i <= 10; i++)
-            {
-                list.Add("Node number " + i);
-            }
-            return list;
-        }
-        #endregion
 
     }
 }
---
+++ AlgorithmsAndDataStructuresUdemy/Tests/LinkedlistTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using AlgorithmsAndDataStructuresUdemy;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LinkedlistTests
+    {
+        [Test]
+        public void GetLastItem()
+        {
+            SingleLinkedList<String> list = GetStringList(20);
+            string lastItemString = list.GetLastItem().data;
+
+            Assert.NotNull(lastItemString);
+        }
+
+        /// <summary>
+        /// Creates and returns a SingleLinkedList full of strings.
+        /// </summary>
+        /// <param name="numberOfItems">Number of items to add.</param>
+        /// <returns></returns>
+        public static SingleLinkedList<String> GetStringList(int numberOfItems)
+        {
+            SingleLinkedList<String> list = new SingleLinkedList<String>();
+            for (int i = 0; i <= numberOfItems; i++)
+            {
+                list.Add($"Item number {i}");
+            }
+            return list;
+        }
+    }
+}
